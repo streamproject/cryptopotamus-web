a3613c22c3ad035e7f069316a0adda0d5f10040c
jessicawatsonmiller <jessicawatsonmiller@gmail.com>
2018-05-23 15:41:49 -0700

    Fixed typos

--- src/assets.d.ts
+++ src/assets.d.ts
@@ -2,6 +2,6 @@ declare module '*.svg'
 declare module '*.png'
 declare module '*.jpg'
 
-// for shitty web3 typings.
+// for web3 typings.
 // https://github.com/ethereum/web3.js/issues/1597
 declare module 'web3'
